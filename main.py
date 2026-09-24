0113f7ad0cb117810ccd64163e69cfe7d4c41af2
Feyza Aydogdu <Feyza.Aydogdu@sbv-fsa.ch>
2026-09-24 15:43:44 +0200

    player whith jump and slide

--- main.py
+++ main.py
@@ -1,24 +1,92 @@
 import pygame
 import os
+import math
 
+## Setup
 pygame.init()
-screen = pygame.display.set_mode((800, 447             ))
+screen = pygame.display.set_mode((800, 447))
 clock = pygame.time.Clock()
-# solange die Variable True ist, soll das Spiel laufen
-spielaktiv = True
 pygame.display.set_caption("Feyzas erstes Phython Game")
+
+
+## Bilder laden
 bg = pygame.image.load(os.path.join('images','bg.png')).convert()
 bgX = 0
 bgX2 = bg.get_width()
 
+
+class player(object):
+    run = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(8, 16)]
+    jump = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(1, 8)]
+    slide = [pygame.image.load(os.path.join('images', 'S1.png')), pygame.image.load(os.path.join('images', 'S2.png')),
+             pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),
+             pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),
+             pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),
+             pygame.image.load(os.path.join('images', 'S3.png')), pygame.image.load(os.path.join('images', 'S4.png')),
+             pygame.image.load(os.path.join('images', 'S5.png'))]
+    jumpList = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
+                4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1,
+                -1, -1, -1, -1, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -3, -3, -3, -3, -3, -3, -3, -3, -3, -3,
+                -3, -3, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4]
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.jumping = False
+        self.sliding = False
+        self.slideCount = 0
+        self.jumpCount = 0
+        self.runCount = 0
+        self.slideUp = False
+
+    def draw(self, win):
+        if self.jumping:
+            self.y -= self.jumpList[self.jumpCount] * 1.2
+            win.blit(self.jump[self.jumpCount // 18], (self.x, self.y))
+            self.jumpCount += 1
+            if self.jumpCount > 108:
+                self.jumpCount = 0
+                self.jumping = False
+                self.runCount = 0
+        elif self.sliding or self.slideUp:
+            if self.slideCount < 20:
+                self.y += 1
+            elif self.slideCount == 80:
+                self.y -= 19
+                self.sliding = False
+                self.slideUp = True
+            if self.slideCount >= 110:
+                self.slideCount = 0
+                self.slideUp = False
+                self.runCount = 0
+            win.blit(self.slide[self.slideCount // 10], (self.x, self.y))
+            self.slideCount += 1
+
+        else:
+            if self.runCount > 42:
+                self.runCount = 0
+            win.blit(self.run[self.runCount // 6], (self.x, self.y))
+            self.runCount += 1
+
+runner = player(200, 313, 64, 64)
+
 def redrawWindow():
     screen.blit(bg, (bgX, 0))  # draws our first bg image
     screen.blit(bg, (bgX2, 0))  # draws the seconf bg image
+    runner.draw(screen)
     pygame.display.update()  # updates the screen
 
-# Call this from the game loop!
+while True:
 
-while spielaktiv:
+    bgX -= 1.4
+    bgX2 -= 1.4
+    if bgX < bg.get_width() * -1:
+        bgX = bg.get_width()
+    if bgX2 < bg.get_width() * -1:
+        bgX2 = bg.get_width()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -28,14 +96,16 @@ while spielaktiv:
                 print("Spieler hat Pfeiltaste rechts gedrückt")
             elif event.key == pygame.K_LEFT:
                 print("Spieler hat Pfeiltaste links gedrückt")
-            elif event.key == pygame.K_UP:
-                print("Spieler hat Pfeiltaste hoch gedrückt")
             elif event.key == pygame.K_DOWN:
-                print("Spieler hat Pfeiltaste runter gedrückt")
-            elif event.key == pygame.K_SPACE:
-                print("Spieler hat Leertaste gedrückt")
+                if not (runner.sliding):
+                    runner.sliding = True
+            elif event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                if not (runner.jumping):
+                    runner.jumping = True
 
     # screen.fill((255, 140, 0))
     redrawWindow()
     clock.tick(60)
+
+run = False
 
